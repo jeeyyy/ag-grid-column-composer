881dd247db41f03df3cf5064674973c6ca32aea0
akash.pandey <akash.pandey@markit.com>
2018-04-03 17:46:21 +0530

    Functionalities Working:

--- src/components/column-composer.ts
+++ src/components/column-composer.ts
@@ -18,7 +18,15 @@ export class ColumnComposer {
         this.addToHiddenColumns($event);
     }.bind(this);
 
-    // TODO: pass parent element when column config should be attached
+    private _onSave = function($event) {
+        // TODO: Return clone?
+        this.onConfigUpdate(this.columns);
+    }.bind(this);
+
+    private _searchColumnList = function($event) {
+        this.onSearch($event);
+    }.bind(this);
+
     constructor(config: IColumnComposer.Configuration) {
         const htmlElement: HTMLElement = document.createElement('DIV');
         htmlElement.innerHTML =
@@ -28,7 +36,7 @@ export class ColumnComposer {
                         ${config.title ? config.title : 'column configuration'}
                     </div>
                     <div class="actions">
-                        <input class="primary-btn" type="button" value="Save" />
+                        <input class="primary-btn" id="btn-save" type="button" value="Save" />
                         <input type="button" value="Cancel"/>
                     </div>
                 </section>
@@ -60,8 +68,7 @@ export class ColumnComposer {
                 </section>
             <section>`;
         this.rootElement = htmlElement.firstChild as HTMLElement;
-        this.onConfigUpdate = config.onConfigUpdate;
-        this._initialise(config.columns);
+        this._initialise(config);
         const parent = config.parentId && document.body.querySelector(`#${config.parentId}`);
         if (!parent) {
             if (console) {
@@ -79,15 +86,48 @@ export class ColumnComposer {
         this.rootElement.classList.add('column-composer-container-hidden')
     }
 
+    _initialise(config: IColumnComposer.Configuration) {
+        this.onConfigUpdate = config.onConfigUpdate;
+        // Save Button
+        this.rootElement.querySelector('.actions #btn-save').addEventListener('click', this._onSave);
+        // Quick Filters
+        debugger;
+        this.rootElement.querySelector('.hidden-column-list-container .quick-filter-container input.quick-filter')
+            .addEventListener('keyup', this._searchColumnList);
+        this.rootElement.querySelector('.visible-column-list-container .quick-filter-container input.quick-filter')
+            .addEventListener('keyup', this._searchColumnList);
+
+        this._initialiseColumns(config.columns);
+    }
+
     refresh(columnDefiniton: IColumnComposer.ColumnDefinition) {
-        this._initialise(columnDefiniton);
+        this._initialiseColumns(columnDefiniton);
     }
 
     destroy() {
-        // TODO: Cleanup        
+        const saveBtn: Element = this.rootElement.querySelector('.actions #btn-save');
+        saveBtn.removeEventListener('click', this._onSave);
+        // TODO: Remove all events
     }
 
-    private _initialise(columns: IColumnComposer.ColumnDefinition) {
+    onSearch($event: Event) {
+        const test = $event.srcElement['value'];
+        const container = $event.srcElement.parentElement.parentElement;
+        if(!test) {
+            (container.querySelectorAll(`.column-list li`) as any).forEach((elem) => {
+                elem.classList.remove('display-none');
+            });
+            return;
+        }
+        (container.querySelectorAll(`.column-list li:not([data-val*="${test.toLowerCase()}"])`) as any).forEach((elem) => {
+            elem.classList.add('display-none');
+        });
+        (container.querySelectorAll(`.column-list li[data-val*="${test.toLowerCase()}"]`) as any).forEach((elem) => {
+            elem.classList.remove('display-none');
+        });
+    }
+
+    private _initialiseColumns(columns: IColumnComposer.ColumnDefinition) {
         // TODO: store clone array?
         this.columns = columns;
 
@@ -177,6 +217,7 @@ export class ColumnComposer {
             });
         }
     }
+
     private _getColIndex(column) {
         const sortedList = this._getSortedListByType(this.columns, column.hide);
         sortedList.forEach((element, index )=> {
@@ -184,6 +225,7 @@ export class ColumnComposer {
         })
 
     }
+
     private onColumnPin($event) {
         const direction: string = $event.srcElement.id.indexOf(`-pin-left`) > -1
             ? `left`
@@ -192,11 +234,31 @@ export class ColumnComposer {
                 : '';
         this.columns
             .filter(column => ($event.srcElement.id === `${column.field}-pin-left`) || ($event.srcElement.id === `${column.field}-pin-right`))
-            .forEach(column => column.pinned = direction);
+            .forEach((column) => {
+                const isPinRemoved: boolean = column.pinned === direction;
+                if(isPinRemoved) {
+                    column.pinned = '';
+                    event.srcElement.classList.remove('active');
+                } else {
+                    column.pinned && $event.srcElement.parentElement.querySelector(`#${column.field}-pin-${column.pinned}`).classList.remove('active');
+                    column.pinned = direction;
+                    event.srcElement.classList.add('active');
+                }
+            });
+    }
+
+    private _findConfigElement(element: Element): Element {
+        let parent: Element = element.parentElement;
+        while (parent && parent.tagName.toLowerCase() !== 'body') {
+            if(parent.classList.contains('column-config')) {
+                return parent;
+            }
+            parent = parent.parentElement;
+        }
+        return null;
     }
 
     // TODO: addToVisibleColumns and addToHiddenColumns can be merged to single function
-    // TODO: Maintain sorting order
     private addToVisibleColumns($event) {
         const configElement = $event.srcElement.parentNode;
         let insertPosition = -1;
@@ -231,11 +293,6 @@ export class ColumnComposer {
         }
     }
 
-    private onSave() {
-        // TODO: Return clone?
-        this.onConfigUpdate(this.columns);
-    }
-
     private _getInsertIndex(column: IColumnComposer.ColumnDefinition, columns: IColumnComposer.ColumnDefinition[]) {
         let index = -1;
         const isHidden: boolean = column.hide;
@@ -255,6 +312,7 @@ export class ColumnComposer {
         const element = document.createElement('LI');
         element.classList.add('column-config');
         element.id = column.field;
+        element.setAttribute('data-val', column.headerName.toLowerCase())
         element.innerHTML =
             `
             ${column.hide
@@ -273,7 +331,7 @@ export class ColumnComposer {
                 <div class="${column.hide ? '' : 'eye'}">
                 </div>
             </div>    
-            <div>${column.headerName ? column.headerName : column.field} ##${column.displayOrder}</div>
+            <div>${column.headerName ? column.headerName : column.field}</div>
         `;
         return element;
     }
